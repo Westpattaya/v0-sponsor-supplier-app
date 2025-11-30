c2aa8368f7221cd6b88298e26e753f5ef1a61ffa
fordoxford <fordoxford@gmail.com>
2025-11-30 15:30:46 +0700

    Fix: Remove missing DashboardHeader import from layout.tsx

--- components/dashboard/layout.tsx
+++ components/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import DashboardSidebar from "./sidebar"
-import DashboardHeader from "./header"
 
 export default function DashboardLayout({
   children,
@@ -12,7 +11,6 @@ export default function DashboardLayout({
     <div className="flex min-h-screen bg-background overflow-x-hidden">
       <DashboardSidebar />
       <div className="flex-1 ml-64 flex flex-col min-w-0">
-        <DashboardHeader />
         <main className="flex-1 overflow-y-auto">
           {children}
         </main>
